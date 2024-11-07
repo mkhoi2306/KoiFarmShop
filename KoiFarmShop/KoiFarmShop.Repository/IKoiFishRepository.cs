208dc75d931105c25f25bc613d5bab6935857f8f
Margarettmini <chaudmse160017@fpt.edu.vn>
2024-11-07 10:21:56 +0700

    UPDATE/repo_service_koifish

---
+++ KoiFarmShop/KoiFarmShop.Repository/IKoiFishRepository.cs
@@ -0,0 +1,12 @@
+﻿using KoiFarmShop.Repository.Models;
+
+namespace KoiFarmShop.Repository;
+
+public interface IKoiFishRepository
+{
+    Task<List<KoiFish>> GetAllAsync();
+    Task<KoiFish> GetByIdAsync(long id);
+    Task AddAsync(KoiFish koiFish);
+    Task UpdateAsync(KoiFish koiFish);
+    Task DeleteAsync(long id);
+}
