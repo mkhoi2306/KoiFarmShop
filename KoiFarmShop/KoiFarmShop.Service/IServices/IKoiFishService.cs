d1b5e22543ff7e55d1dbf9e544a528ed94d1571c
Margarettmini <chaudmse160017@fpt.edu.vn>
2024-11-08 15:42:58 +0700

    UPDATE/Repo_Service

--- KoiFarmShop/KoiFarmShop.Service/IServices/IKoiFishService.cs
+++ KoiFarmShop/KoiFarmShop.Service/IServices/IKoiFishService.cs
@@ -5,7 +5,7 @@ namespace KoiFarmShop.Service;
 public interface IKoiFishService
 {
     Task<IEnumerable<KoiFish>> GetAllKoiFishAsync();
-    Task<KoiFish> GetKoiFishByIdAsync(int id);
+    Task<KoiFish> GetKoiFishByIdAsync(long id);
     Task AddKoiFishAsync(KoiFish koiFish);
     Task UpdateKoiFishAsync(KoiFish koiFish);
     Task DeleteKoiFishAsync(int id);
--- KoiFarmShop/KoiFarmShop.WebApp/Pages/Staff/Delete.cshtml.cs
+++ KoiFarmShop/KoiFarmShop.WebApp/Pages/Staff/Delete.cshtml.cs
@@ -56,7 +56,7 @@ namespace KoiFarmShop.WebApp.Pages.Staff
                 await _context.SaveChangesAsync();
             }
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("/Staff/Index");
         }
     }
 }
